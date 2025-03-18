7d26c0656b4cc5f9990b89d4f747dada771483b5
Hoon94 <dleognsdl1@naver.com>
2025-03-18 23:25:47 +0900

    feat: Project Setup

---
+++ Monthly/MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Daehoon Lee on 3/18/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
