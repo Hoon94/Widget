efac95036072cef98efb3e083f6c2c84c9b66b5e
Hoon94 <dleognsdl1@naver.com>
2025-03-30 23:59:37 +0900

    feat: Project Setup

---
+++ RepoWatcher/RepoWatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepoWatcher
+//
+//  Created by Daehoon Lee on 3/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepoWatcher/RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Daehoon Lee on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
