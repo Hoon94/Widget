021a3b189776ac73dc583a43c313bba4bcc1be81
Hoon94 <dleognsdl1@naver.com>
2025-04-22 23:32:37 +0900

    feat: App Overview - Project & Core Data Setup

---
+++ SwiftCal/SwiftCal/Persistence.swift
@@ -0,0 +1,45 @@
+//
+//  Persistence.swift
+//  SwiftCal
+//
+//  Created by Daehoon Lee on 4/22/25.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    @MainActor
+    static let preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+            let newDay = Day(context: viewContext)
+            newDay.date = Date()
+            newDay.didStudy = Bool.random()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "SwiftCal")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
